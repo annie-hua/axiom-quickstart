f66c05ac60fd7987fa7c880e4aa88ce80c2feb37
Raja Shravan <rajashravan@hotmail.com>
2024-01-07 20:13:20 -0500

    wip

--- app/app/components/BuildQuery.tsx
+++ app/app/components/BuildQuery.tsx
@@ -26,21 +26,31 @@ export default function BuildQuery({
     setParams(inputs, callbackAddress, callbackExtraData);
   }, [setParams, inputs, callbackAddress, callbackExtraData]);
 
+  const buildQuery = async () => {
+    console.log("buildQuery called")
+    if (!areParamsSet) {
+      console.log("params not set")
+      return;
+    }
+    console.log("building query")
+    await build();
+  };
+
   useEffect(() => {
-    const buildQuery = async () => {
-      if (!areParamsSet) {
-        return;
-      }
-      await build();
-    };
     buildQuery();
   }, [build, areParamsSet]);
 
   if (!builtQuery) {
     return (
+      <>
       <div className="flex flex-row items-center font-mono gap-2">
         {"Building Query"} <LoadingAnimation />
       </div>
+
+      <div onClick={buildQuery} className="flex flex-row items-center font-mono gap-2">
+        build
+        </div>
+        </>
     );
   }
 }
